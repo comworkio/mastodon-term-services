85247b1e17b585f1b2e5ef29d5d62628537a5436
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-02 13:13:03 +0100

    Update the date

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -3,6 +3,16 @@
 REPO_PATH="${PROJECT_HOME}/mastodon-term-services/"
 
 cd "${REPO_PATH}" && git pull origin main || :
+
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_EMAIL}"
+
+current_date="$(date +%Y-%m-%d-T%H:%M:%S%z)"
+sed -i "s/\(Last update: \)[^\.]*\./\1${current_date}./g" terms.html
+git add .
+git commit -m "Update date terms.html: ${current_date}"
+
+git push origin main
 git push github main -f
 git push pgitlab main -f
 git push froggit main -f
