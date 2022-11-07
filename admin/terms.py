9603069f06aa0a3a7f6730066425b2e036c925da
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-07 15:51:38 +0100

    Inline file

--- admin/terms.py
+++ admin/terms.py
@@ -1,5 +1,5 @@
 from utils.logger import log_msg
-from utils.file import file_get_contents
+from utils.file import file_get_contents_inline
 from utils.db import sql_execute
 
 import os
@@ -7,4 +7,4 @@ import os
 sql = "UPDATE settings SET updated_at = NOW(), value = %s WHERE var = 'site_terms'"
 
 log_msg("INFO", "Updating term of uses")
-sql_execute(sql, "--- {}".format(file_get_contents(os.environ['TERMS_FILE_PATH'])))
+sql_execute(sql, "--- {}".format(file_get_contents_inline(os.environ['TERMS_FILE_PATH'])))
--- admin/utils/file.py
+++ admin/utils/file.py
@@ -1,2 +1,2 @@
-def file_get_contents(path):
-    return ''.join(open(path, 'r').readlines()).rstrip()
+def file_get_contents_inline(path):
+    return ''.join(open(path, 'r').readlines()).rstrip().replace("\n", "").replace("\r", "")
