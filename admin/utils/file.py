63653e2f38664a4af8b4e9b3ac516b254936c86a
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-07 16:43:15 +0100

    Fix: term of use formatting

--- admin/utils/file.py
+++ admin/utils/file.py
@@ -2,4 +2,4 @@ def file_get_contents_inline(path):
     return ''.join(open(path, 'r').readlines()).rstrip().replace("\n", "").replace("\r", "")
 
 def config_settings(path):
-    return "--- \"{}\"".format(file_get_contents_inline.replace('"', r'\"'))
+    return "--- \"{}\"".format(file_get_contents_inline(path).replace('"', r'\"'))
