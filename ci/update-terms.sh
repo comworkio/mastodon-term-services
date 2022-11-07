4da191af54edcc5be0eb83a66876663df1953f12
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-07 15:25:46 +0100

    Fix: env vars

--- ci/update-terms.sh
+++ ci/update-terms.sh
@@ -4,11 +4,11 @@ source ./ci/compute-env.sh
 
 ENV_FILE="./admin/mastodon-terms.env"
 
-echo "${LOG_LEVEL}" > "${ENV_FILE}"
-echo "${DB_HOST}" >> "${ENV_FILE}"
-echo "${DB_PORT}" >> "${ENV_FILE}"
-echo "${DB_NAME}" >> "${ENV_FILE}"
-echo "${DB_USER}" >> "${ENV_FILE}"
-echo "${DB_PASSWORD}" >> "${ENV_FILE}"
+echo "LOG_LEVEL=${LOG_LEVEL}" > "${ENV_FILE}"
+echo "DB_HOST=${DB_HOST}" >> "${ENV_FILE}"
+echo "DB_PORT=${DB_PORT}" >> "${ENV_FILE}"
+echo "DB_NAME=${DB_NAME}" >> "${ENV_FILE}"
+echo "DB_USER=${DB_USER}" >> "${ENV_FILE}"
+echo "DB_PASSWORD=${DB_PASSWORD}" >> "${ENV_FILE}"
 
 docker-compose -f ./admin/docker-compose.yml up --build --force-recreate update-tems
