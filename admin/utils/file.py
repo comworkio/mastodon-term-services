8ab217057acd3138961ae744ab9ec8432879bce7
Idriss Neumann <idriss.neumann@comwork.io>
2022-11-20 23:21:15 +0100

    Formatting markdown

--- admin/utils/file.py
+++ admin/utils/file.py
@@ -1,5 +1,5 @@
-def file_get_contents(path):
-    return ''.join(open(path, 'r').readlines())
+def file_get_contents(path, separator = ''):
+    return separator.join(open(path, 'r').readlines())
 
 def file_get_contents_inline(path):
     return file_get_contents(path).rstrip().replace("\n", "").replace("\r", "")
@@ -8,4 +8,4 @@ def format_html_settings(path):
     return "--- \"{}\"".format(file_get_contents_inline(path).replace('"', r'\"'))
 
 def format_md_settings(path):
-    return "--- \"{}\"".format(file_get_contents(path))
+    return "--- \"{}\"".format(file_get_contents(path, '\n'))
